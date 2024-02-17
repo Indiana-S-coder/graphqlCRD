402015f74695f2590b1643202486051ad2071199
Bharti Patel <patelshree40@gmail.com>
2024-02-17 17:17:27 +0530

    index file changes made

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
-const {ApolloServer} = require("apollo-server");
+const express = require("express")
+const {ApolloServer} = require("apollo-server-express");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 
+const app = express();
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 })
 
+server.applyMiddleware({app})
 const PORT = process.env.PORT || 5000;
 
 mongoose
 .connect(process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB connected successfully!")
-    return server.listen(PORT);
+    return app.listen(PORT);
 })
 .then((res) => {
     console.log(`Server running at ${res.url}`)
